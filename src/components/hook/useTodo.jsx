2f2495cd9aaea801ee4bd6972e9c2051b4976ba8
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-09 00:00:25 -0500

    UseTodo hook added

---
+++ src/components/hook/useTodo.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodo = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (newTodo) => {
+    if (newTodo.desc.trim().length >= 1) {
+      dispatch({
+        type: "add",
+        payload: newTodo,
+      });
+    }
+  };
+
+  const deleteTodo = (id, e) => {
+    e.preventDefault();
+
+    const action = {
+      type: "delete",
+      payload: id,
+    };
+
+    dispatch(action);
+  };
+
+  const handleToggle = (todoId) => {
+    dispatch({
+      type: "toggle",
+      payload: todoId,
+    });
+  };
+
+  return { todos, addTodo, deleteTodo, handleToggle };
+};
